16180fc8afc26551c8b3b85e82c74934a18b7393
Yashiro <1161569428@qq.com>
2022-11-24 15:22:31 +0800

    Mainnet to parachain

---
+++ src/api/parachain.ts
@@ -0,0 +1,17 @@
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { logger } from '@polkadot/util';
+import { parachainEndpoint } from '../env';
+const l = logger('parachain-api');
+
+export const parachainApi: ApiPromise = new ApiPromise({
+    provider: new WsProvider(parachainEndpoint || 'ws://localhost:9944')
+});
+
+parachainApi.on('connected', () => {
+    l.log(`PARACHAIN API has been connected to the endpoint: ${parachainEndpoint}`)
+})
+
+parachainApi.on('disconnected', (): void => {
+    l.error('API has been disconnected from the endpoint')
+    process.exit(0)
+});
--- src/env.ts
+++ src/env.ts
@@ -4,8 +4,8 @@
 require('dotenv').config();
 
 export const port = process.env.port
-export const maxwellChainEndpoint = process.env.MAXWELL_CHAIN_ENDPOINT as string;
-export const shadowChainEndpoint = process.env.SHADOW_CHAIN_ENDPOINT as string;
+export const mainnetChainEndpoint = process.env.MAINNET_CHAIN_ENDPOINT as string;
+export const parachainEndpoint = process.env.PARACHAIN_ENDPOINT as string;
 export const seeds = process.env.SEEDS as string;
 export const latestBlock = Number(process.env.LATESTBLOCK)
 export const destId = Number(process.env.DESTID)
